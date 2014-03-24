9b8afb939031aaa08794b36a1a7aed2a844e7f10
hengjiaqi <hengjiaqi@gmail.com>
2014-03-24 15:07:34 -0700

    Add main page

---
+++ mainPage.h
@@ -0,0 +1,13 @@
+//
+//  mainPage.h
+//  sandbox
+//
+//  Created by jake on 3/24/14.
+//  Copyright (c) 2014 jake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainPage : UIViewController
+
+@end
