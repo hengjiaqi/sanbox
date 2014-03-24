8c13ae15a65eb5adcf0e847675df2b522ac8b9d3
wuyue <yue10owen@gmail.com>
2014-03-24 13:25:16 -0700

    Auto scroll keyboard in the login page
    wuyue’s first commit

--- sandbox/logInPage.m
+++ sandbox/logInPage.m
@@ -34,7 +34,8 @@ NSString *twilioNumber = @"4257287464";
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
                                    initWithTarget:self
                                    action:@selector(dismissKeyboard)];
-    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:)name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:)name:UIKeyboardDidHideNotification object:nil];
     [self.view addGestureRecognizer:tap];
     password.secureTextEntry = YES;
     phoneNumber.delegate = self;
@@ -116,6 +117,22 @@ NSString *twilioNumber = @"4257287464";
     
     [textField resignFirstResponder];
     return YES;
+}
+-(void)keyboardDidShow:(NSNotification *)notification{
+    if ([[UIScreen mainScreen]bounds].size.height==568) {
+        [self.view setFrame:CGRectMake(0, -50, 320, 568)];
+    }else{
+        [self.view setFrame:CGRectMake(0, -130, 320, 480)];
+    }
+}
+
+
+-(void)keyboardDidHide:(NSNotification *)notification{
+    if ([[UIScreen mainScreen]bounds].size.height==568) {
+        [self.view setFrame:CGRectMake(0, 0, 320, 568)];
+    }else{
+        [self.view setFrame:CGRectMake(0, 0, 320, 480)];
+    }
 }
 
 @end
