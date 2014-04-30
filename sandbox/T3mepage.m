1d4da446be3617718405d2cd0f5c4c319622cc2d
hengjiaqi <hengjiaqi@gmail.com>
2014-04-29 22:07:43 -0700

    update on me page

---
+++ sandbox/T3mepage.m
@@ -0,0 +1,175 @@
+//
+//  mepageTableViewController.m
+//  sandbox
+//
+//  Created by wuyue on 3/28/14.
+//  Copyright (c) 2014 jake. All rights reserved.
+//
+
+#import "T3mepage.h"
+#import <AWSSimpleDB/AWSSimpleDB.h>
+#import "AmazonClientManager.h"
+//#import "FriendListTableViewController.m"
+NSString *USER_NAME;
+NSString *GNickname;
+@interface T3mepage ()
+
+@end
+
+@implementation T3mepage
+@synthesize me_NickName_textfield = _me_NickName_textfield;
+@synthesize me_Password_textfield = _me_Password_textfield;
+@synthesize me_rePassword_textfield = _me_rePassword_textfield;
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+  //  AmazonSimpleDBClient *sdb = [AmazonClientManager sdb];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+     [self.view addGestureRecognizer:tap];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    USER_NAME = [defaults objectForKey:@"EAT2GETHER_ACCOUNT_NAME"];
+    SimpleDBGetAttributesRequest *gar = [[SimpleDBGetAttributesRequest alloc] initWithDomainName:USER_NAME andItemName:@"nicknameItem"];
+    SimpleDBGetAttributesResponse *response = [[AmazonClientManager sdb] getAttributes:gar];
+    for (SimpleDBAttribute *attr in response.attributes) {
+        NSLog(@"nickname here is %@", attr.value);
+        _me_NickName_textfield.text = attr.value;
+    }
+
+    
+    //me_NickName_textfield.delegate = self;
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+/*
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+-(void)dismissKeyboard {
+    [_me_NickName_textfield resignFirstResponder];
+    [_me_Password_textfield resignFirstResponder];
+    [_me_rePassword_textfield resignFirstResponder];
+}
+- (IBAction)me_back_button:(id)sender {
+    [self dismissViewControllerAnimated:NO completion:nil];
+
+}
+
+- (IBAction)me_update_button:(id)sender {
+     NSLog(@"NickName text field%@", _me_NickName_textfield.text);
+     NSLog(@"password text field%@", _me_Password_textfield.text);
+     NSLog(@"repassword text field%@", _me_rePassword_textfield.text);
+    AmazonSimpleDBClient *sdb = [AmazonClientManager sdb];
+    // get the nickname on the database
+    SimpleDBGetAttributesRequest *gar = [[SimpleDBGetAttributesRequest alloc] initWithDomainName:USER_NAME andItemName:@"nicknameItem"];
+    SimpleDBGetAttributesResponse *response = [[AmazonClientManager sdb] getAttributes:gar];
+    for (SimpleDBAttribute *attr in response.attributes) {
+          NSLog(@"nickname here is %@", attr.value);
+    }
+    SimpleDBReplaceableAttribute *nickNameAttribute = [[SimpleDBReplaceableAttribute alloc] initWithName:@"nicknameAttribute" andValue:_me_NickName_textfield.text andReplace:YES];
+    NSMutableArray *NicknameAttributes = [[NSMutableArray alloc] initWithCapacity:1];
+    [NicknameAttributes addObject:nickNameAttribute];
+    
+    SimpleDBPutAttributesRequest *putAttributesRequest = [[SimpleDBPutAttributesRequest alloc] initWithDomainName: USER_NAME andItemName:@"nicknameItem" andAttributes:NicknameAttributes];
+    [sdb putAttributes:putAttributesRequest];
+    response = [[AmazonClientManager sdb] getAttributes:gar];
+    
+    for (SimpleDBAttribute *attr in response.attributes) {
+        NSLog(@"nickname here is after update %@", attr.value);
+        GNickname =attr.value;
+    }
+}
+@end
